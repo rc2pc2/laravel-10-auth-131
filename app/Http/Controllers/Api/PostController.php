aab7cab7688298841ea21e1093588cea2fa2966d
rc2pc2 <petriccarcc@gmail.com>
2024-11-14 14:04:29 +0100

    added index api route for Post resource

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+
+    public function index(){
+        $posts = Post::with("category", "user", "tags", "user.posts")->get();
+        return response()->json([
+                "success" => true,
+                "results" => $posts,
+        ]);
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\PostController;
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get("/posts", [PostController::class, "index"])->name("api.posts.index");
